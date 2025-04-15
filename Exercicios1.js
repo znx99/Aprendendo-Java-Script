6e4d8a413a9990b1a906af10c4c399d0fa83223d
Guilherme Barros Zanella <zanella.guilherme501@gmail.com>
2025-04-14 21:21:43 -0300

    Create Exercicios1.js

---
+++ Exercicios1.js
@@ -0,0 +1,9 @@
+//1)Declare três variáveis diferentes (uma para cada tipo: string, número e booleano) 
+// e atribua valores a elas. Em seguida, exiba o tipo de cada variável no console.
+const estudante = "zanella"
+const inteiro = 1
+const adm = false
+
+console.log(typeof estudante)
+console.log(typeof inteiro)
+console.log(typeof adm)
